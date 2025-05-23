c7a880466bdecd41b4dfc2b1ec5c394e3cba7d98
Ahmedgito <mamooabid1@gmail.com>
2025-05-23 06:04:30 +0500

    this

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const cookieParser = require("cookie-parser");
 const express = require("express");
+const app = express();
+
 const path = require('path')
-const app = express() ;
-const userModel = require('./models/user') ; 
+const userModel = require('./models/user');
+const cookieParser = require("cookie-parser");
 
-
-app.set('view engine' , 'ejs') ;
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({ extended : true })); 
-app.use(express.static(path.join(__dirname , 'public')));
-app.use(cookieParser()) ;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 
 app.get("/", (req, res) => {
@@ -18,18 +18,20 @@ app.get("/", (req, res) => {
 
 });
 
+
+
 app.post("/create", async (req, res) => {
 
-    let { username , age , password , email } = req.body ; 
+    let { username, age, password, email } = req.body;
 
     let createdUser = await userModel.create({
-        username ,
-        age ,
+        username,
+        age,
         password,
         email,
-    }) ;
+    });
 
-    res.send(createdUser) ;
+    res.send(createdUser);
 
 });
 
